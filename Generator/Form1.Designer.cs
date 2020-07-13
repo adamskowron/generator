7715942c73f677f43b6eea041c54ace9bf746741
Adam Skowronski <236036@student.pwr.edu.pl>
2020-07-13 02:45:18 +0200

    final

---
+++ Generator/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1118, 621);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 640);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 640);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1142, 708);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "BESTIA Z WADOWIC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Generator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generator
+{
+    public partial class Form1 : Form
+    {
+
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+    
+
+        
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int counter = int.Parse(textBox2.Text);
+
+            Random random1 = new Random();
+            StringBuilder builder = new StringBuilder();
+
+            char ch;
+
+
+            for (int i = 0; i < counter; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
+                    builder.Append(ch);
+                }
+
+                string city = builder.ToString();
+                builder.Clear();
+
+
+
+                for (int j = 0; j < 6; j++)
+                {
+                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
+                    builder.Append(ch);
+                }
+
+                string postalCode = builder.ToString();
+                builder.Clear();
+
+                for (int j = 0; j < 35; j++)
+                {
+                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
+                    builder.Append(ch);
+                }
+
+                string road = builder.ToString();
+                builder.Clear();
+
+                for (int j = 0; j < 5; j++)
+                {
+                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
+                    builder.Append(ch);
+                }
+
+                string number = builder.ToString();
+                builder.Clear();
+
+                string query = "INSERT INTO Adres(Kod_pocztowy,Miasto,Ulica,Numer_domu)\n";
+                query += $" VALUES ('{postalCode}','{city}','{road}','{number}');\n";
+
+                textBox1.AppendText(query);
+            }
+        }
+    }
+}
