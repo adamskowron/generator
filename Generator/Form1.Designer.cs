58b0c62250f9b9144e42050eff58d499cac4374a
Adam Skowronski <236036@student.pwr.edu.pl>
2021-03-26 18:56:00 +0100

    upgraded version

--- Generator/Form1.Designer.cs
+++ Generator/Form1.Designer.cs
@@ -28,48 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.textBoxQueryCounter = new System.Windows.Forms.TextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.listBoxValues = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.labelINSERT = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxDataType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxLenght = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonCopy = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(11, 10);
+            this.textBoxOut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxOut.Size = new System.Drawing.Size(614, 545);
+            this.textBoxOut.TabIndex = 0;
+            // 
+            // textBoxQueryCounter
+            // 
+            this.textBoxQueryCounter.Location = new System.Drawing.Point(652, 331);
+            this.textBoxQueryCounter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxQueryCounter.Name = "textBoxQueryCounter";
+            this.textBoxQueryCounter.Size = new System.Drawing.Size(171, 22);
+            this.textBoxQueryCounter.TabIndex = 1;
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(836, 325);
+            this.buttonGenerate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(153, 34);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // listBoxValues
+            // 
+            this.listBoxValues.FormattingEnabled = true;
+            this.listBoxValues.ItemHeight = 16;
+            this.listBoxValues.Location = new System.Drawing.Point(729, 76);
+            this.listBoxValues.Name = "listBoxValues";
+            this.listBoxValues.Size = new System.Drawing.Size(173, 132);
+            this.listBoxValues.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(864, 214);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(110, 30);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // labelINSERT
+            // 
+            this.labelINSERT.AutoSize = true;
+            this.labelINSERT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelINSERT.Location = new System.Drawing.Point(631, 24);
+            this.labelINSERT.Name = "labelINSERT";
+            this.labelINSERT.Size = new System.Drawing.Size(137, 25);
+            this.labelINSERT.TabIndex = 8;
+            this.labelINSERT.Text = "INSERT INTO";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
+            this.textBox1.Location = new System.Drawing.Point(775, 24);
             this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(1118, 621);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.Size = new System.Drawing.Size(149, 22);
+            this.textBox1.TabIndex = 9;
             // 
-            // textBox2
+            // comboBoxDataType
             // 
-            this.textBox2.Location = new System.Drawing.Point(23, 640);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 26);
-            this.textBox2.TabIndex = 1;
+            this.comboBoxDataType.FormattingEnabled = true;
+            this.comboBoxDataType.Items.AddRange(new object[] {
+            "INT",
+            "STRING"});
+            this.comboBoxDataType.Location = new System.Drawing.Point(737, 212);
+            this.comboBoxDataType.Name = "comboBoxDataType";
+            this.comboBoxDataType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxDataType.TabIndex = 12;
+            this.comboBoxDataType.Text = "INT";
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(222, 640);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 43);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(631, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "VALUES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(752, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "data type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(658, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "lenght";
+            // 
+            // textBoxLenght
+            // 
+            this.textBoxLenght.Location = new System.Drawing.Point(636, 214);
+            this.textBoxLenght.Name = "textBoxLenght";
+            this.textBoxLenght.Size = new System.Drawing.Size(95, 22);
+            this.textBoxLenght.TabIndex = 15;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(908, 120);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(102, 34);
+            this.buttonRemove.TabIndex = 7;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(652, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Number of queries";
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(737, 408);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(174, 63);
+            this.buttonCopy.TabIndex = 17;
+            this.buttonCopy.Text = "Copy to clipboard";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1142, 708);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(1015, 566);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxLenght);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxDataType);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelINSERT);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxValues);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textBoxQueryCounter);
+            this.Controls.Add(this.textBoxOut);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "BESTIA Z WADOWIC";
+            this.Text = "INSERT INTO GENERATOR";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -78,9 +221,21 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.TextBox textBoxQueryCounter;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.ListBox listBoxValues;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label labelINSERT;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxDataType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxLenght;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCopy;
     }
 }
 
--- Generator/Form1.cs
+++ Generator/Form1.cs
@@ -13,82 +13,107 @@ namespace Generator
 {
     public partial class Form1 : Form
     {
-
-
-        
-
         public Form1()
         {
             InitializeComponent();
-
         }
-    
-
-        
-
-        
-
+  
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBoxOut.ReadOnly = true;
+            textBoxOut.Multiline = true;
+            textBoxOut.AcceptsReturn = true;
+            comboBoxDataType.DropDownStyle = ComboBoxStyle.DropDownList;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonGenerate_Click(object sender, EventArgs e)
         {
-            textBox1.Clear();
-            int counter = int.Parse(textBox2.Text);
-
-            Random random1 = new Random();
-            StringBuilder builder = new StringBuilder();
+            textBoxOut.Clear();
+            int counter = int.Parse(textBoxQueryCounter.Text);
+            textBoxQueryCounter.ReadOnly = false;
 
             char ch;
-
-
+            Random random1 = new Random();
+            StringBuilder builder = new StringBuilder();
+            
             for (int i = 0; i < counter; i++)
             {
-                for (int j = 0; j < 20; j++)
+                builder.Append($"INSERT INTO {textBox1.Text} VALUES (");
+                foreach(string item in listBoxValues.Items)
                 {
-                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
-                    builder.Append(ch);
+                    String[] words = item.Split('/');
+                    
+                    if(words[0].Equals("INT"))
+                    {
+                        for (int j = 0; j< Int32.Parse( words[1]) ; j++)
+                        {
+                            int num = random1.Next(0,9);
+                            builder.Append(num.ToString());
+                        }
+                        builder.Append(",");
+                        
+                    }
+                    else
+                    {
+                        builder.Append("\"");
+                        for (int j = 0; j < Int32.Parse(words[1]); j++)
+                        {
+                            builder.Append(Convert.ToChar(
+                                Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65))));
+                        }
+                        builder.Append("\"");
+                        builder.Append(",");
+                    }
                 }
+                builder.Length -= 1;
+                builder.Append(");" + Environment.NewLine);
+                
 
-                string city = builder.ToString();
-                builder.Clear();
-
-
-
-                for (int j = 0; j < 6; j++)
-                {
-                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
-                    builder.Append(ch);
-                }
-
-                string postalCode = builder.ToString();
-                builder.Clear();
-
-                for (int j = 0; j < 35; j++)
-                {
-                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
-                    builder.Append(ch);
-                }
-
-                string road = builder.ToString();
-                builder.Clear();
-
-                for (int j = 0; j < 5; j++)
-                {
-                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random1.NextDouble() + 65)));
-                    builder.Append(ch);
-                }
-
-                string number = builder.ToString();
-                builder.Clear();
-
-                string query = "INSERT INTO Adres(Kod_pocztowy,Miasto,Ulica,Numer_domu)\n";
-                query += $" VALUES ('{postalCode}','{city}','{road}','{number}');\n";
-
-                textBox1.AppendText(query);
             }
+            textBoxOut.AppendText(builder.ToString());
+         
+            builder.Clear();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int lenght = -1;
+            if (int.TryParse(textBoxLenght.Text, out lenght))
+            {
+                if (comboBoxDataType.Text.Equals("INT"))
+                {
+                    listBoxValues.Items.Add("INT/" + textBoxLenght.Text);
+
+                }
+                else if (comboBoxDataType.Text.Equals("STRING"))
+                {
+                    listBoxValues.Items.Add("STRING/" + textBoxLenght.Text);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect input", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection selectedItems = new ListBox.SelectedObjectCollection(listBoxValues);
+            selectedItems = listBoxValues.SelectedItems;
+
+            if (listBoxValues.SelectedIndex != -1)
+            {
+                for (int i = selectedItems.Count - 1; i >= 0; i--)
+                    listBoxValues.Items.Remove(selectedItems[i]);
+            }
+            else
+                MessageBox.Show("No item selected");
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(textBoxOut.Text);
         }
     }
 }
